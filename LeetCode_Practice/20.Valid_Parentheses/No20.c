ccf58edacc75b46909ca5ccadf5c1e759b054326
IceAI <jason3234@gmail.com>
2025-08-26 14:54:57 +0000

    Solve LeetCode No.20 with improved solution

--- LeetCode_Practice/20.Valid_Parentheses/No20.c
+++ LeetCode_Practice/20.Valid_Parentheses/No20.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool isValid(char* s) {
     // To ignore the space complexity, use array to simulate stack FILO feature
 
-    char stack[10000];
-    int size = 0;
+    char stack[strlen(s)];  // Fix#1: use strlen to get length, not declare maximum size of array
+    int top = -1;  // Fix#2: To minus 1, reduce time for the minus behavior
 
     while(*s!='\0'){
         // If char is left side, push it.
@@ -18,44 +19,23 @@ bool isValid(char* s) {
         // Shall consider the corner case of size==0 but face right side.
 
         if(*s == '(' || *s == '[' || *s == '{'){
-            stack[size] = *s;
-            size++;
+            stack[++top] = *s;
         }
         else{
-            if(size == 0){
-                return false;
-            }
-            else if(*s == ')'){
-                if(stack[size-1] == '('){
-                    size--;
-                }
-                else{
+            // Fix#3: Combined condition to make code be more clean
+            if(top == -1 ||
+                (*s == ')' && stack[top]!='(') ||
+                (*s == ']' && stack[top]!='[') ||
+                (*s == '}' && stack[top]!='{')){
                     return false;
                 }
-            }
-            else if(*s == ']'){
-                if(stack[size-1] == '['){
-                    size--;
-                }
-                else{
-                    return false;
-                }
-            }
-            else if(*s == '}'){
-                if(stack[size-1] == '{'){
-                    size--;
-                }
-                else{
-                    return false;
-                }
+            else{
+                top--;
             }
         }
         s++;
     }
-    if(size != 0){
-        return false;
-    }
-    return true;
+    return top==-1;  // Fix#4: Removed the if to make code be more clean
 }
 
 
