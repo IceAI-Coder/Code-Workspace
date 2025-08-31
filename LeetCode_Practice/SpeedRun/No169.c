0e41472a67c4f6522266d838fa660efa50fbbd40
IceAI <jason3234@gmail.com>
2025-08-31 14:42:49 +0000

    Change No.169 to Boyer–Moore Majority Vote Algorithm

--- LeetCode_Practice/SpeedRun/No169.c
+++ LeetCode_Practice/SpeedRun/No169.c
@@ -1,30 +1,21 @@
 // https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
 
-int compare_integers(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
 
 int majorityElement(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), compare_integers);
-    
-    if(numsSize == 1){
-        return nums[0];
-    }
-
-    int count = 1;
-    int curr = nums[0];
+    int idx, count;
+    idx = 0;
+    count = 1;
     for(int i=1; i<numsSize; i++){
-        if(nums[i] == curr){
+        if(nums[idx] == nums[i]){
             count++;
         }
         else{
-            curr = nums[i];
+            count--;
+        }
+        if(count == 0){
+            idx = i;
             count = 1;
         }
-
-        if(count * 2 >= numsSize){
-            return nums[i];
-        }
     }
-    return -1;
+    return nums[idx];
 }
