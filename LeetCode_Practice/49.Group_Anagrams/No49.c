5b032b63d45f6813599d82e46c11ed410e5d6954
IceAI <jason3234@gmail.com>
2025-09-02 15:17:07 +0000

    Solve LeetCode No.49 (with AI suggestion)

--- LeetCode_Practice/49.Group_Anagrams/No49.c
+++ LeetCode_Practice/49.Group_Anagrams/No49.c
@@ -3,175 +3,128 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define HASH_SEED 10007  // 1 <= strs.length <= 10^4
+// Issue#1 Hash function to weak. 
+//  - Change to FNV hash (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
+//  - Use golden value (https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine)
+// Issue#2 Hash table size to close strs.length will cause O(n^2) when finding
+// Issue#3 Need to free hash table and so on
 
-typedef struct hashElement{
-    struct hashElement* next;
-    char* data_ptr;          // point to the *strs
-} hashElement;
+#define HASH_TABLE_SIZE 20003 // 1 <= strs.length <= 10^4
 
-typedef struct hashList{
-    hashElement* HEAD;
+typedef struct Node{
+    struct Node* next;
+    char* data;
+    int sig[26];
+} Node;
+
+typedef struct Hash_Table{
+    Node* HEAD;
     int listSize;
-}hashList;
+} Hash_Table;
 
-// Time Complexity O(char length)
-bool isAnagrams(const char* a, const char* b){
-    int alphabetStore[26], idx;
+void caculate_26_Signature(const char* input, int sig[26]){
+    memset(sig, 0, sizeof(int)*26);
+    while(*input){
+        sig[*input++ - 'a']++;
+    }
+}
+
+bool is_SignatureMatch(int sig1[26], int sig2[26]){
     for(int i=0; i<26; i++){
-        alphabetStore[i] = 0;
-    }
-    while(*a != '\0'){
-        alphabetStore[*a++ - 'a']++;
-    }
-    while(*b != '\0'){
-        idx = *b - 'a';
-        alphabetStore[idx]--;
-        if(alphabetStore[idx] < 0){
-            return false;
-        }
-        b++;
-    }
-    for(int i=0; i<26; i++){
-        if(alphabetStore[i] != 0){
+        if(sig1[i] != sig2[i]){
             return false;
         }
     }
     return true;
 }
 
-
-int hash(const char* a){
-    // formula 
-    // output = (char length) * sum(char)
-    int len = 0;
-    int output = 0;
-    while(a[len] != '\0'){
-        output+= a[len] - 'a' + 1;  // plus 1 to let a have more impact for output
-        len++;
+int caculate_hash(int sig[26]){
+    unsigned h = 0x01000193;  // FNV_offset_basis
+    for(int i=0; i<26; i++){
+        h ^= (unsigned)sig[i] + 0x9e3779b9 + (h<<6) + (h>>4);  // Golden value for seed. Use <<6 & >>4 for high/low byte mix
     }
-    return (output * len) % HASH_SEED;
+    return h % HASH_TABLE_SIZE;
 }
+
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
+
 char*** groupAnagrams(char** strs, int strsSize, int* returnSize, int** returnColumnSizes) {
-    hashList* hashTable = (hashList*)calloc(HASH_SEED, sizeof(hashList));
+    Hash_Table* hashTable = (Hash_Table*)calloc(HASH_TABLE_SIZE, sizeof(Hash_Table));
     *returnSize = 0;
 
+    Node* nodeElement;
     int insertIndex;
     for(int i=0; i<strsSize; i++){
-        insertIndex = hash(strs[i]);
+        nodeElement = (Node*)malloc(sizeof(Node));
+        nodeElement->data = strs[i];
+        caculate_26_Signature(nodeElement->data, nodeElement->sig);
+        insertIndex = caculate_hash(nodeElement->sig);
         while(hashTable[insertIndex].HEAD != NULL){
-            if(isAnagrams(strs[i], hashTable[insertIndex].HEAD->data_ptr)){
+            // this hash value already used
+            if(is_SignatureMatch(hashTable[insertIndex].HEAD->sig, nodeElement->sig)){
                 break;
             }
-            insertIndex++;
+            // HASH Value Collision so plus 1
+            insertIndex = (insertIndex + 1 ) % HASH_TABLE_SIZE;
         }
-        // insert
         if(hashTable[insertIndex].HEAD == NULL){
             (*returnSize)++;
         }
-        hashElement* newData = (hashElement*)malloc(sizeof(hashElement));
-        newData->data_ptr = strs[i];
-        newData->next = hashTable[insertIndex].HEAD;
-        hashTable[insertIndex].HEAD = newData;
+        nodeElement->next = hashTable[insertIndex].HEAD;
+        hashTable[insertIndex].HEAD = nodeElement;
         hashTable[insertIndex].listSize++;
-    }
+    }    
 
-    for(int i=0; i<HASH_SEED; i++){
-        if(hashTable[i].listSize > 0){
-            hashElement* ptr;
-            ptr = hashTable[i].HEAD;
-            printf("[i=%d]  ", i);
-            while(ptr!=NULL){
-                printf("%s, ", ptr->data_ptr);
-                ptr = ptr->next;
-            }
-            printf("\n");
-        }
-    }
-
-
-
-
-
-    // char*   point to string
-    // char**  point to string array
-    // char*** point to array of string array
     char*** output = (char***)malloc(sizeof(char**)*(*returnSize));
-    char**  outputStringArray;
-
-    // int*   point to int
-    // int**  point to int array
     *returnColumnSizes = (int*)malloc(sizeof(int)*(*returnSize));
-    int count = 0;
-    hashElement* ptr;
-    for(int i=0; i<HASH_SEED; i++){
-        if(hashTable[i].listSize > 0){
-            (*returnColumnSizes)[count] = hashTable[i].listSize;
+    int count = 0;   // sync for *returnSize
+    int count2 = 0;  // sync for ** returnColumnSizes
+    Node* ptr;       // point to the data would like to store to output
+    for(int i=0; i<HASH_TABLE_SIZE; i++){
+        if(hashTable[i].listSize != 0){
+            count2 = 0;
 
-            ptr = hashTable[i].HEAD;
-            outputStringArray = (char**)malloc(sizeof(char*)*(hashTable[i].listSize));
-            for(int j=0; j<hashTable[i].listSize; j++){
-                outputStringArray[j] = ptr->data_ptr;
-                ptr = ptr->next;
+            ptr = hashTable[i].HEAD;            
+            (*returnColumnSizes)[count] = hashTable[i].listSize;
+            output[count] = (char**)malloc(sizeof(char*)*hashTable[i].listSize);
+            while(ptr!=NULL){
+                output[count][count2++] = ptr->data;
+                hashTable[i].HEAD = hashTable[i].HEAD->next;
+                free(ptr);
+                ptr = hashTable[i].HEAD;
             }
-            output[count] = outputStringArray;
             count++;
         }
     }
     return output;
 }
 
-int main(){
-    char* subStr;
-    char** str = (char**)malloc(sizeof(char*)*6);
-    subStr = (char*)malloc(sizeof(char)*3);
-    subStr = "eat";
-    str[0] = subStr;
-
-    subStr = (char*)malloc(sizeof(char)*3);
-    subStr = "tea";
-    str[1] = subStr;
-
-    subStr = (char*)malloc(sizeof(char)*3);
-    subStr = "tan";
-    str[2] = subStr;
-
-    subStr = (char*)malloc(sizeof(char)*3);
-    subStr = "ate";
-    str[3] = subStr;
-
-    subStr = (char*)malloc(sizeof(char)*3);
-    subStr = "nat";
-    str[4] = subStr;
-
-    subStr = (char*)malloc(sizeof(char)*3);
-    subStr = "bat";
-    str[5] = subStr;
+int main() {
+    char* str[] = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    int strsSize = sizeof(str) / sizeof(str[0]);
 
     int returnSize = 0;
     int* returnColumnSizes;
-    char*** output = groupAnagrams(str,6, &returnSize, &returnColumnSizes);
+    char*** output = groupAnagrams(str, strsSize, &returnSize, &returnColumnSizes);
 
-    printf("%d\n", returnSize);
-    for(int i=0; i<returnSize; i++){
-        printf("returnColumnSizes = %d\n", returnColumnSizes[i]);
-    }
-
-    for(int i=0; i<returnSize; i++){
+    for (int i = 0; i < returnSize; i++) {
         printf("[");
-        for(int j=0; j<returnColumnSizes[i]; j++){
+        for (int j = 0; j < returnColumnSizes[i]; j++) {
             printf("\"%s\"", output[i][j]);
-            if(j!=returnColumnSizes[i]-1){
+            if (j != returnColumnSizes[i] - 1) {
                 printf(", ");
             }
         }
         printf("]\n");
+        free(output[i]);
     }
+    free(output);
+    free(returnColumnSizes);
     return 0;
 }
